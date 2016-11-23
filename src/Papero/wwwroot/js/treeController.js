2c4f8d4a1200532d1e3dffb6a9c297b17607bf38
Giorgio Borgo <giorgio.borgo@gmail.com>
2016-11-23 17:12:08 +0100

    Albero e tabella abbastanza funzionanti

--- src/Papero/wwwroot/js/treeController.js
+++ src/Papero/wwwroot/js/treeController.js
@@ -1,4 +1,4 @@
-﻿//treeController.js nuova versione
+﻿//treeController.js funzionante
 
 (function () {
     "use strict";
@@ -8,47 +8,75 @@
 
     function treeController($http) {
 
+        var sottospecieSelezionate = [];  // Array delle foglie selezionate all'interno dell'albero
+        var elencoEsemplari = [];  // Elenco completo non filtrato degli esemplari 
+
         var vm = this;
+        vm.tassonomia = [];  // Albero tassonomico
+        vm.esemplariSelezionati = [];  // Contenuto della tabella 
+        vm.treeOptions = {
+            allowDeselect: false,
+            dirSelectable: true,
+            nodeChildren: "figli",
+            injectClasses: {
+                ul: "a1",
+                li: "a2",
+                liSelected: "a7",
+                iExpanded: "a3",
+                iCollapsed: "a4",
+                iLeaf: "a5",
+                label: "a6",
+                labelSelected: "a8"
+            }
+        };
 
+        vm.nodoSelezionato = function selezionaEsemplari(nodo) {
+            var elencoFigli = [];
+            function trovaFigli(nodo) {
 
-        vm.nodoSelezionato = function prova(node) {
-            alert(node.id);
+                if (!!nodo.figli) {
+                    for (var figlio in nodo.figli) {
+                        trovaFigli(nodo.figli[figlio]);
+                    }
+                }
+                else {
+                    elencoFigli[elencoFigli.length] = nodo.id;
+                }
+                return elencoFigli;
+            }
+
+            if (!!nodo.specieId) {
+                elencoFigli = nodo.id;
+            } else {
+                elencoFigli = trovaFigli(nodo);
+            };
+
+            //alert(sottospecieSelezionate);
+
+            sottospecieSelezionate = elencoFigli;
+
+            vm.esemplariSelezionati = _.filter(elencoEsemplari, function (esemplare) { return esemplare.sottospecieId in sottospecieSelezionate; });
+
+            //vm.esemplariSelezionati = [{ "id": 1, "sottospecieId": 1791, "msng": 58364 }];
+
         }
 
         $http.get("/api/esemplari")
              .then(function (response) {
-                 //$scope.esemplari = response.data;
-
-
-                 vm.esemplari = [
-                       { "id": 1, "sottospecieId": 1791, "msng": 58364 }
-                 ];
-
-
-
-
+                 elencoEsemplari = response.data;
              });
 
+
         $http.get("/api/famiglie")
-        .then(function (response) {
+            .then(function (response) {
 
-            vm.treeOptions = {
-                dirSelectable: true,
-                injectClasses: {
-                    ul: "a1",
-                    li: "a2",
-                    liSelected: "a7",
-                    iExpanded: "a3",
-                    iCollapsed: "a4",
-                    iLeaf: "a5",
-                    label: "a6",
-                    labelSelected: "a8"
-                }
-            };
+                vm.tassonomia = response.data;
+            });
 
-            vm.tassonomia = response.data;
 
-        });
+
+
+
 
 
     }
