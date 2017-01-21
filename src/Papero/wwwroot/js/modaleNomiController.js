61179f707824ee960881cd52c710edb0f947e80d
Giorgio Borgo <giorgio.borgo@gmail.com>
2017-01-21 16:33:48 +0100

    non più necessari gli input nascosti

--- src/Papero/wwwroot/js/modaleNomiController.js
+++ src/Papero/wwwroot/js/modaleNomiController.js
@@ -12,9 +12,15 @@
 
         vm.elencoStatiConservazione = [];
         vm.statoConservazioneSelezionato = null;
+        vm.nomeItaliano = "";
+        vm.nomeInglese = "";
 
 
         vm.apriModaleNomi = function caricaStatiConservazione() {
+
+            vm.nomeItaliano = $("#nomeItalianoDD").text();
+            vm.nomeInglese = $("#nomeIngleseDD").text();
+
             $http.get("/api/staticonservazione")
              .then(function (response) {
                  vm.elencoStatiConservazione = response.data;              
