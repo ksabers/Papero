bd9aded3087f4173d18a4956292f18ffa28145eb
Giorgio Borgo <giorgio.borgo@gmail.com>
2017-06-23 17:55:53 +0200

    Aggiunta del controllo Angular alla pagina del profilo utente

---
+++ src/Papero/wwwroot/js/auth/profiloController.js
@@ -0,0 +1,30 @@
+﻿//profiloController.js 
+
+(function () {
+    "use strict";
+
+    angular.module("papero-app")
+        .controller("profiloController", profiloController);
+
+    function profiloController() {
+        var vm = this;
+
+        vm.inputInsertNome = $("#inputInsertNome").val();
+        vm.inputInsertCognome = $("#inputInsertCognome").val();
+        vm.inputInsertEmail = $("#inputInsertEmail").val();
+        vm.inputInsertTelefono = $("#inputInsertTelefono").val();
+
+        vm.verificaInsertPassword = function verificaInsertPassword() {
+
+            if (_.trim(vm.inputInsertPassword) != _.trim(vm.inputConfermaPassword)) {
+                vm.pulsanteInsertDisabilitato = true;
+            }
+            else {
+                vm.pulsanteInsertDisabilitato = false;
+            };
+
+        };
+
+    }
+
+})();
